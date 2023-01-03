bcb82bca9234b2cff978ddabecbea1ecbd140edb
Arnav Mehta <arnavdatt@gmail.com>
2023-01-03 11:55:47 +0000

    Added project

---
+++ UntitledStealthGame/Source/UntitledStealthGame/UntitledStealthGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UntitledStealthGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNTITLEDSTEALTHGAME_API AUntitledStealthGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
